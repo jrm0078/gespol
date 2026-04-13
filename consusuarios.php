5bd9523a459292c2700f871c61b6e159ff87c3d7
Jrm0078 <jrm0078@alu.medac.es>
2026-04-13 18:45:23 +0200

    Encuadre en usuarios

--- consusuarios.php
+++ consusuarios.php
@@ -1,99 +1,54 @@
-<?php include("inc/seguridad.php"); ?> 	
+<?php include("inc/seguridad.php"); ?> 
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Catalweb">
-    <meta name="author" content="">
-    <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <title>Consulta de Usuarios</title>
+<!-- ============================================================== -->
+<!-- Preloader - style you can find in spinners.css -->
+<!-- ============================================================== -->
+<div class="preloader2">
+	<div class="lds-ripple">
+		<div class="lds-pos"></div>
+		<div class="lds-pos"></div>
+	</div>
+</div>
 
+<!-- ============================================================== -->
+<!-- Start Page Content -->
+<!-- ============================================================== -->
+<div class="container-fluid">
+	<div class="row mb-3">
+		<div class="col-12">
+			<button type="button" id="btnCrear" class="btn btn-success btn-rounded">
+				<i class="fa fa-star"></i> Crear Usuario
+			</button>
+		</div>
+	</div>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
-
-<body>
-
-			<!-- ============================================================== -->
-			<!-- Preloader - style you can find in spinners.css -->
-			<!-- ============================================================== -->
-			<div class="preloader2">
-				<div class="lds-ripple">
-					<div class="lds-pos"></div>
-					<div class="lds-pos"></div>
+	<div class="row">
+		<div class="col-12">
+			<div class="card">
+				<div class="card-body">
+					<div class="table-responsive">
+						<table id="zero_config" class='display' style='width:100%'>
+							<thead>
+								<tr>
+									<th style="width: 50px;"></th>
+									<th>Cod. Usuario</th>
+									<th>Nombre</th>
+									<th>Email</th>
+									<th>Rol</th>
+								</tr>
+							</thead>
+							<tbody>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
+		</div>
+	</div>
+</div>
+<!-- ============================================================== -->
+<!-- End Container fluid -->
+<!-- ============================================================== -->
 
-            <div class="container-fluid">
-                <!-- ============================================================== -->
-                <!-- Start Page Content -->
-                <!-- ============================================================== -->
-                <div class="card-body">
-					<button type="button" id="btnCrear" class="btn btn-success btn-rounded"><i class="fa fa-star"></i> Crear Usuario</button>				
-                </div>
-
-				<div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table id="zero_config" class='display compact' style='width:100%'>
-                                        <thead>
-                                            <tr>
-                                                <th></th>												
-                                                <th>Cod. Usuario</th>
-                                                <th>Nombre</th>
-												<th>email</th>
-												<th>rol</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        </tbody>
-                                        <tfoot>
-                                            <tr>
-
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-				
-            <!-- ============================================================== -->
-            <!-- End Container fluid  -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- footer -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- End footer -->
-            <!-- ============================================================== -->
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Page wrapper  -->
-        <!-- ============================================================== -->
-
-    <!-- ============================================================== -->
-    <!-- End Wrapper -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
-
-	
-    <!-- this page js -->
-	<script src="js/consusuarios.js"></script>
-</body>
-
-</html>
+<!-- this page js -->
+<script src="js/consusuarios.js"></script>
